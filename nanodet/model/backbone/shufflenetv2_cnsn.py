d607546ec0cd2bac3a4a7928335c4318afbe6d2c
samcw <tsukihie@gmail.com>
2022-02-22 17:50:06 +0800

    update

--- nanodet/model/backbone/shufflenetv2_cnsn.py
+++ nanodet/model/backbone/shufflenetv2_cnsn.py
@@ -89,7 +89,7 @@ class ShuffleV2Block(nn.Module):
         )
 
         # pos in ['residual', 'pre', 'post', 'identity']
-        pos = 'identity'
+        pos = 'pre'
         self.pos = pos
         cnsn_type = 'cnsn'
 
